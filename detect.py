fe528d167f9f8266162cea1231b5a92d302bfc04
Dominik Wójtowicz <89572059+baldmaan@users.noreply.github.com>
2023-01-07 23:24:11 +0100

    added to detect.py color ranges and finding methods

--- detect.py
+++ detect.py
@@ -5,7 +5,73 @@ from typing import Dict
 import click
 import cv2
 from tqdm import tqdm
+import numpy as np
+# kernel
 
+kernel = np.ones((7,7),np.uint8)
+
+ # color ranges
+lower_green = np.array([33, 80, 0])
+upper_green = np.array([61, 255, 255])
+
+lower_red1 = np.array([0, 97, 32])
+upper_red1 = np.array([8, 255, 255])
+
+lower_red2 = np.array([160, 72, 32])
+upper_red2 = np.array([179, 255, 255])
+
+lower_purple = np.array([110, 50, 0])
+upper_purple = np.array([168, 255, 235])
+
+lower_yellow = np.array([20, 125, 92])
+upper_yellow = np.array([32, 255, 255])
+
+def create_mask(hsv, hsv_down, hsv_up):
+    mask = cv2.inRange(hsv, hsv_down, hsv_up)
+    erosion = cv2.erode(mask, kernel, iterations=3)
+    grad = cv2.dilate(erosion, kernel, iterations=3)
+    return grad
+
+
+def find_contours(mask):
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    count = 0
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        count += 1
+    return count
+
+def green(hsv):
+    mask = create_mask(hsv, hsv_down=lower_green, hsv_up=upper_green)
+    amount = find_contours(mask=mask)
+    # print('green', amount)
+    # maskShow = cv2.resize(mask, (width, height))
+    # cv2.imshow("Green", maskShow)
+    return amount
+
+def purple(hsv):
+    mask = create_mask(hsv, hsv_down=lower_purple, hsv_up=upper_purple)
+    amount = find_contours(mask=mask)
+    # print('purple', amount)
+    # maskShow = cv2.resize(mask, (width, height))
+    # cv2.imshow("Purple", maskShow)
+    return amount
+
+def yellow(hsv):
+    mask = create_mask(hsv, hsv_down=lower_yellow, hsv_up=upper_yellow)
+    amount = find_contours(mask=mask)
+    # print('yellow', amount)
+    # maskShow = cv2.resize(mask, (width, height))
+    # cv2.imshow("Yellow", maskShow)
+    return amount
+
+def red(hsv):
+    mask1 = create_mask(hsv,hsv_down=lower_red1, hsv_up=upper_red1)
+    amount = find_contours(mask=mask1)
+    # mask2 = create_mask(hsv,hsv_down=lower_red2, hsv_up=upper_red2)
+    # amount += find_contours(mask=mask2)
+    # print('red', amount)
+    return amount
 
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
@@ -21,15 +87,19 @@ def detect(img_path: str) -> Dict[str, int]:
         Dictionary with quantity of each object.
     """
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    image = cv2.GaussianBlur(img, (9, 9), 0)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
     #TODO: Implement detection method.
-    
-    red = 0
-    yellow = 0
-    green = 0
-    purple = 0
+    amount_green = green(hsv)
+    amount_purple = purple(hsv)
+    amount_yellow = yellow(hsv)
+    amount_red = red(hsv)
 
-    return {'red': red, 'yellow': yellow, 'green': green, 'purple': purple}
+    results = {'red': amount_red, 'yellow': amount_yellow, 'green': amount_green, 'purple': amount_purple}
+    print(results)
+
+    return results
 
 
 @click.command()
@@ -46,6 +116,7 @@ def main(data_path: Path, output_file_path: Path):
 
     with open(output_file_path, 'w') as ofp:
         json.dump(results, ofp)
+
 
 
 if __name__ == '__main__':
